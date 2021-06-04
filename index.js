23cc2b06c998a2969da4e1ef05f0e4324912786e
masontmorris <70476475+masontmorris@users.noreply.github.com>
2021-06-04 05:02:27 -0400

    Set volume to half

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const weezer = document.querySelector('.weezer');
 const newAudio = new Audio('/sounds/lol.mp3');
 
-
+newAudio.volume = .5;
 weezer.onmouseover = ()=>{
   console.log('hell0')
   newAudio.play();
